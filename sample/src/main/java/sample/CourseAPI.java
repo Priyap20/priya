3c6620b5083676b0423f6e59916aac9a2b8d54fe
abhi20Priya <jadhavpriya403@gmail.com>
2019-07-06 11:28:53 +0530

    first

---
+++ sample/src/main/java/sample/CourseAPI.java
@@ -0,0 +1,13 @@
+package sample;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPI {
+
+	public static void main(String ...a) {
+
+		SpringApplication.run(CourseAPI.class, a);
+	}
+
+}
